420bf3575a572eec54b312b99a1f949eb3240e45
b9labCM <b9lab.mtlcrl@outlook.it>
2018-08-31 09:12:39 +0000

    contract refactor changed logic and fixes

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -1,319 +1,169 @@
 pragma solidity ^0.4.24;
 
 import "./Stoppable.sol";
-import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+
 contract RockPaperScissors is Stoppable {
-    
-    uint private gamePassword = 1234; 
-    bytes32 private h1 = computeHash(1,gamePassword);
-    bytes32 private h2 = computeHash(1,gamePassword);
-    bytes32 private h3 = computeHash(1,gamePassword);
-    
-    struct Player {
-        address addrPlayer;
-        uint move; // 0:none, 1:rock, 2:paper, 3:Scissors
-        bool status;
-        bool inGame;
-        uint wins;
-        bytes32 secretHand;
-    }
+
+    enum Hand {NONE, ROCK, PAPER, SCISSORS}
     
     struct Game {
-        uint gameId;
-        Player player1;
-        Player player2;
-        uint requiredAmount;
-        uint gameBalance;
-    }
+        address player1;
+        address player2;
         
-    mapping(uint=>Game) public games;
-    mapping(address => uint) public balances; 
-    
+        uint requiredAmount;
+
+        bytes32 secretHand1;
+        Hand Hand2;
+
+        uint expiringTime;
+
+        uint balance; // not so useful maybe...
+    }
+
+    mapping (address => uint) public balances;
+    mapping (uint  => Game) games;
+
     event LogDeposit(address sender, uint amount);
-    event LogJoinGame(uint indexed gameId, address player);
-    event LogGame(uint indexed gameId, uint amount);
-    event LogPlay(uint move1, uint move2, uint win, address indexed winner);
-    event LogExit(address sender, uint gameId);
+    event LogCreateGame(uint gameId, uint requiredAmount, bytes32 secretHand1);
+    event LogJoinGame(uint gameId, address player);
+    event LogPlay(Hand move1, Hand move2, address player1, address player2, address indexed winner, bytes32 secretHand1);
     event LogWithdraw(address indexed sender, uint amount);
-    
-    constructor () public { }
-    
+
+    constructor () public {}
+
     /* 0. Deposits funds to became a player */
-    function deposit() public payable returns(bool success) {
+    function deposit() public payable returns(bool success){
         require(msg.value > 0, "Insufficient funds, please deposits something");
         require(msg.sender != address(0), "Address must not be 0x0");
         emit LogDeposit(msg.sender,  msg.value);
         balances[msg.sender] += msg.value; 
         return true;
     }
-    
-    /* 1. Create a game.*/
-    function createGame(uint gameId, uint amount) public onlyIfRunning returns(bool success) {
-        require(games[gameId].gameId == 0, "A game with this name already exist!");
-        require(amount >= 0, "The amount has to be positive");
+    /* 1. Create Game  */
+    function createGame(uint _gameId, uint _requiredAmount, bytes32 _secretHand1, uint duration) external onlyIfRunning returns(bool success){
+        require(games[_gameId].player1 == address(0), "The game already exist!");
+        require(balances[msg.sender] >= _requiredAmount, "Insufficient funds to join the game");
         
-        Player memory p1 = Player({
-                                    addrPlayer: address(0),
-                                    move: 0,
-                                    status: false,
-                                    inGame: false,
-                                    wins: 0,
-                                    secretHand: ""
-                                });
-        Player memory p2 = Player({
-                                    addrPlayer: address(0),
-                                    move: 0,
-                                    status: false,
-                                    inGame: false,
-                                    wins: 0,
-                                    secretHand: ""
-                                });
-        
-        emit LogGame(gameId, amount);
-        games[gameId] = Game({ 
-                                gameId: gameId,
-                                player1: p1,
-                                player2: p2,
-                                requiredAmount: amount,
-                                gameBalance: 0
-                            });
-        
+        emit LogCreateGame(_gameId,_requiredAmount, _secretHand1);
+        uint _expiringTime = now + duration; //use safemath
+
+        games[_gameId] = Game({ 
+            player1: msg.sender,
+            player2: address(0),
+            requiredAmount: _requiredAmount,
+            secretHand1: _secretHand1,
+            Hand2: Hand.NONE,
+            expiringTime: _expiringTime,
+            balance: _requiredAmount
+            });
+
+        balances[msg.sender] -= _requiredAmount;
+     
         return true;
     }
-    
-    /* 1. Join an existing game. You have to deposit the right amount to join the game.*/
-    function joinGame(uint gameId) public onlyIfRunning returns(bool success){
-        require(games[gameId].gameId != 0, "This game does not exist");
-        require(balances[msg.sender]  >= games[gameId].requiredAmount , "Insufficient funds to join this game!");
-        require(msg.sender != address(0), " address must not be 0x0");
-        require(msg.sender != owner, " address must not be the owner");
-        require(games[gameId].player1.status == false || games[gameId].player2.status == false, "You can join only one game");
-        
-        emit LogJoinGame(gameId, msg.sender);
-        
-        // Enroll and enable players to play 
-        if ( games[gameId].player1.status == false ){
-            games[gameId].player1.status = true;
-            games[gameId].player1.addrPlayer = msg.sender;
-            
-        } else if ( games[gameId].player2.status == false ) {
-            require(games[gameId].player1.addrPlayer != msg.sender ); 
-            games[gameId].player2.status = true;
-            games[gameId].player2.addrPlayer = msg.sender;
-            
-        } else {
-            revert();
-        }
-        
-        //Update balances
-        balances[msg.sender] -= games[gameId].requiredAmount;
-        games[gameId].gameBalance += games[gameId].requiredAmount; 
-        
+    /* 2. Join game: another player can join the game created */
+    function joinGame(uint8 _gameId, Hand _move2) external  onlyIfRunning returns(bool success){
+        require(now <= games[_gameId].expiringTime, "");
+        require(games[_gameId].player1 != address(0), "");
+        require(msg.sender != games[_gameId].player1, "");
+        require(balances[msg.sender] >= games[_gameId].requiredAmount, "");
+        require(_move2 != Hand.NONE, "You should play");
+
+        balances[msg.sender] -= games[_gameId].requiredAmount;
+		
+        emit LogJoinGame(_gameId, msg.sender);
+
+        games[_gameId].player2 = msg.sender;
+        games[_gameId].Hand2 = _move2;
+        games[_gameId].balance += games[_gameId].requiredAmount;
+
         return true;
-    }
-    
-    /* 3. Set a move before calling playGame function,
-            or skip this and call playRandom function.
-    */
-    function playYourHand(uint gameId, bytes32 myHand) public onlyIfRunning returns( bool success ) {
-        require(games[gameId].gameId != 0, "This game does not exist, check the ID");
-        require(myHand != 0, "Make a move"); 
-	    address tmpP1 = games[gameId].player1.addrPlayer;
-	    address tmpP2 = games[gameId].player2.addrPlayer; 
-        require(msg.sender == tmpP1 || msg.sender == tmpP2, "You have to join this game first!" );
-        // Set the player's hand
-	    if ( msg.sender == tmpP1 ){
-        	games[gameId].player1.secretHand = myHand;
-            games[gameId].player1.move = unveilHand(myHand);
-	    } else if ( msg.sender == tmpP2 ){
-        	games[gameId].player2.secretHand = myHand;
-        	games[gameId].player2.move = unveilHand(myHand);
-	    }   
-        return true;
-    }
-    
-    
-    /* 4. Function to simulate a play game: hands are randomnly generated.*/  
-    function playRandom(uint gameId) public onlyIfRunning onlyOwner returns( address winner) {
-        require(games[gameId].gameId != 0, "This game does not exist, check the ID");
-        require(games[gameId].gameBalance == games[gameId].requiredAmount*2, "Players have not deposited the right amount");
-        
-        uint tmpAmount = games[gameId].gameBalance;
-        games[gameId].gameBalance = 0;
-        
-        games[gameId].player1.move = randn(1,3);
-        games[gameId].player2.move = randn2(1,3);
-        uint move1 = games[gameId].player1.move;
-        uint move2 = games[gameId].player2.move;
-        
-        address tmpWinner;
-        uint winnerIndex = compare(move1,move2);
-        
-        if (winnerIndex == 1) {
-            games[gameId].player1.wins +=1;
-            
-            //witdraw
-            tmpWinner = games[gameId].player1.addrPlayer;
-            withdraw(tmpWinner, tmpAmount);
-        }
-        else if(winnerIndex == 2) {
-            games[gameId].player2.wins +=1;
-            
-            //witdraw
-            tmpWinner = games[gameId].player2.addrPlayer;
-            withdraw(tmpWinner, tmpAmount);
-            
-        } else {
-            //pareggio rigioca
-            revert();
-        }
-        
-        emit LogPlay(move1, move2, winnerIndex, tmpWinner);
-        
-        // Delete the game once it has been played
-        delete games[gameId];
-        
-        return tmpWinner;
     }
 
-    /* 5. Function to play the game: hands are previously set.*/
-    function playGame(uint gameId) public onlyIfRunning onlyOwner returns( address winner) {
-        require(games[gameId].gameId != 0, "This game does not exist, check the ID");
-        require(games[gameId].gameBalance == SafeMath.mul(games[gameId].requiredAmount,2));
-        
-        uint tmpAmount = games[gameId].gameBalance;
-        games[gameId].gameBalance= 0;
-        
-        uint move1 = games[gameId].player1.move;
-        uint move2 = games[gameId].player2.move;
-        
-        address tmpWinner;
-        uint winnerIndex = compare(move1,move2);
-        
-        if (winnerIndex == 1) {
-            games[gameId].player1.wins +=1;
-            
-            //witdraw
-            tmpWinner = games[gameId].player1.addrPlayer;
-            balances[tmpWinner] += tmpAmount;
-            withdraw(tmpWinner, tmpAmount);
-        }
-        else if(winnerIndex == 2) {
-            games[gameId].player2.wins +=1;
-            
-            //witdraw
-            tmpWinner = games[gameId].player2.addrPlayer;
-            withdraw(tmpWinner, tmpAmount);
-            
-        } else {
-            //pareggio rigioca
-            tmpWinner = 0x0;
-        }
-        delete games[gameId];
-        emit LogPlay(move1, move2, winnerIndex, tmpWinner);
-        return tmpWinner;
+    /* 3. Play game: compare the two hands and select a winner */
+    function playGame (uint _gameId, uint _password1, Hand _move1) external onlyIfRunning returns(address winnerAddr, bool success){
+        require(now <= games[_gameId].expiringTime, "");
+        require(games[_gameId].player2 != address(0), "");
+        require(_move1 != Hand.NONE, "You should choice");
+        require(games[_gameId].balance == games[_gameId].requiredAmount+games[_gameId].requiredAmount, ""); // use safemath
+		
+        bytes32 hash1 = computeHash(_password1, _move1);
+        require (hash1 == games[_gameId].secretHand1, "Player one had changed his move...");
+		
+        uint winner = compare(_move1, games[_gameId].Hand2);
+        if(winner == 1){
+            balances[games[_gameId].player1] += 2*games[_gameId].requiredAmount;
+            winnerAddr = games[_gameId].player1;
+		}else if (winner == 2){
+			balances[games[_gameId].player1] += 2*games[_gameId].requiredAmount;
+			winnerAddr = games[_gameId].player2;
+		}else{
+			balances[games[_gameId].player1] += games[_gameId].requiredAmount;
+			balances[games[_gameId].player2] += games[_gameId].requiredAmount;
+			winnerAddr = address(0);
+			success = false;	
+		}
+        emit LogPlay(_move1, games[_gameId].Hand2, games[_gameId].player1, games[_gameId].player2, winnerAddr, hash1);
+        delete games[_gameId];
+        success = true;
     }
-    
-    /* In progress Function to play the game: hands are previously set.
-    function playGameOnly(uint gameId) public onlyIfRunning onlyOwner returns( address winner) {
-        require(games[gameId].gameId != 0, "This game does not exist, check the ID");
-        require(games[gameId].gameBalance == SafeMath.mul(games[gameId].requiredAmount,2));
-        
-        uint tmpAmount = games[gameId].gameBalance;
-        games[gameId].gameBalance= 0;
-        
-        uint move1 = games[gameId].player1.move;
-        uint move2 = games[gameId].player2.move;
-        
-        address tmpWinner;
-        uint winnerIndex = compare(move1,move2);
-        
-        if (winnerIndex == 1) {
-            games[gameId].player1.wins +=1;
-            tmpWinner = games[gameId].player1.addrPlayer;
-        }
-        else if(winnerIndex == 2) {
-            games[gameId].player2.wins +=1;
-            tmpWinner = games[gameId].player2.addrPlayer;
-        } else {
-            //pareggio rigioca
-            revert();
-        }
-        balances[tmpWinner] += tmpAmount;
-        emit LogPlay(move1, move2, winnerIndex, tmpWinner);
-        return tmpWinner;
-    } */
-    
-    /* Function to call to stop play --in progress --do not use it yet! 
-       ...Searching a way to let players play many times in the same game.
-       ...Call withdraw function only when a player exit the game
-       ...Check if the player can leave the game 
-    */
-    function exitGame(uint gameId) public returns(bool success) {
-        require(msg.sender == games[gameId].player1.addrPlayer || msg.sender == games[gameId].player2.addrPlayer);
-        withdraw2();
-        delete games[gameId];
-        return true;
-    }
-    
-    function withdraw(address receiver, uint amount) internal onlyIfRunning returns(bool success){
-        require(amount>0);
-        require(balances[receiver] > 0);
-        require(amount <= balances[receiver]);
-        emit LogWithdraw(msg.sender, amount);
-        balances[receiver] = 0; 
-        receiver.transfer(amount);
-        return true; 
-    }
-    
-    function withdraw2() internal onlyIfRunning returns(bool success){
-        require(balances[msg.sender] > 0);
+
+	/* Withdraw */
+    function withdraw() external returns(bool success){
+        require(balances[msg.sender] > 0, "Nothing to withdraw...");
+               
         uint amount = balances[msg.sender];
         balances[msg.sender] = 0;
         emit LogWithdraw(msg.sender, amount);
         msg.sender.transfer(amount);
-        return true; 
+
+        return true;
     }
-    
-    /*Utils*/
-    
-    function randn(uint from, uint to) public view returns(uint) {
-        return uint(keccak256(abi.encodePacked(block.timestamp)))%to +from;
-    }
-    
-    function randn2(uint from, uint to) public view returns(uint) {
-        return uint(keccak256(abi.encodePacked(block.timestamp,block.difficulty)))%to +from;
-    }
-    
-    function computeHash(uint password1, uint password2) public pure returns(bytes32){
-        return keccak256(abi.encodePacked(password1,password2));
-    }
-    /* Map of hashed hands */
-    function unveilHand(bytes32 hand) internal view onlyIfRunning returns (uint move){
-        uint tmpHand;
-        if (hand == h1){
-            tmpHand = 1;
-        } else if (hand == h2){
-            tmpHand = 2;
-        } else if (hand == h3){
-            tmpHand = 3;
+
+    // Only arrange balances without transfer any amount. Use withdraw to do that
+    function withdrawBack(uint _gameId) public returns(bool success){
+        // You can take back money only if the session is expired
+        require(now > games[_gameId].expiringTime, "ok...Session expired");
+        require(games[_gameId].secretHand1 != 0x0, "");
+        require(msg.sender == games[_gameId].player1 || msg.sender == games[_gameId].player2, "You are not a player of this game");
+        //require(balances[msg.sender] > 0, "Nothing to withdraw...");
+        // keep trace of withrawable amount
+        uint withdrawableAmount = games[_gameId].balance;
+        games[_gameId].balance = 0;
+
+        // P1 create the game but nobody join the game within the deadline -> P1 takes back his money
+        if (msg.sender == games[_gameId].player1 && games[_gameId].player2 == address(0)){
+            balances[games[_gameId].player1] += withdrawableAmount;
+        // P1 e P2 join the game but they do not play within the deadline -> P1 and P2 take back their money 
+        } else if (msg.sender == games[_gameId].player2 ){
+            balances[games[_gameId].player1] += withdrawableAmount/2;  //use safemath
+            balances[games[_gameId].player2] += withdrawableAmount/2;
+            // The remainder shoud be zero since players deposit the same amount to join the game 
         }
-        return tmpHand;
+        
+        return true;
     }
-    
-    function compare(uint move1, uint move2) internal pure returns (uint win) {
+
+    /*UTILS*/ 
+    //Helpers
+    function computeHash(uint password, Hand move) public pure returns(bytes32){
+        return keccak256(abi.encodePacked(password,move));
+    }
+
+    //1-rock ; 2-paper ; 3-scissors
+    function compare(Hand move1, Hand move2) internal pure returns(uint win){
         if (move1 == move2) return 0;
-        if ( (move1 == 1 && move2==2) || 
-             (move1 == 3 && move2==2) ||
-             (move1 == 3 && move2==1) ) return 2;
-        if ( (move1 == 1 && move2==3) || 
-             (move1 == 2 && move2==1) ||
-             (move1 == 2 && move2==3) ) return 1;
-         
+        if ( (move1 == Hand.ROCK && move2==Hand.PAPER) || 
+             (move1 == Hand.PAPER && move2==Hand.SCISSORS) ||
+             (move1 == Hand.SCISSORS && move2==Hand.ROCK) ) return 2;
+        if ( (move1 == Hand.ROCK && move2==Hand.SCISSORS) || 
+             (move1 == Hand.PAPER && move2==Hand.ROCK) ||
+             (move1 == Hand.SCISSORS && move2==Hand.PAPER) ) return 1;
     }
-    
-    
-    
+
+
+
     
 }
 
